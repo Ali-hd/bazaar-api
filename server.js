83141ff8ddfb231734e5812cd2c436f9d8032f1c
Ali-hd <ali.hamidaddin1@gmail.com>
2020-04-18 21:23:02 +0300

    removed netlify

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const passport = require('passport');
 const methodOverride = require('method-override') 
 require('./passport');
-const serverless = require('serverless-http')
 
 var authRout = require('./routes/auth')
 var userRout = require('./routes/user')
@@ -18,8 +17,8 @@ server.use(express.urlencoded({ extended: false }))
 server.use(cors())
 server.use(passport.initialize())
 
-server.use('/.netlify/functions/server/auth',authRout)
-server.use('/.netlify/functions/server/post',postRout)
+server.use('/auth',authRout)
+server.use('/post',postRout)
 // server.use('/user', userRout)
 
 server.use(methodOverride('_method'))
@@ -34,4 +33,4 @@ const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`))
 
-module.exports.handler = serverless(server);
+module.exports = server;
